0e3879b4a5f929fc0d0e359ec363a328f14f9011
ryanguilherme <ryanguilhermetbt@gmail.com>
2025-07-15 02:38:42 -0300

    Receiver: distributing the data properly in respective variables

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -11,8 +11,8 @@
 RF24 radio(2, 4);  // CE, CSN
 const byte address[6] = "00001";
 boolean switchstate = 0;
-static const int PITCH;
-static const int YAW;
+static const int PITCH = 27;
+static const int YAW = 26;
 
 Servo pitch;
 Servo yaw;
@@ -20,25 +20,33 @@ Servo yaw;
 char dataReceived[32]; // this must match dataToSend in the TX
 bool newData = false;
 
+// global variables to store LY, RX, RT, DPAD, and BUTTON
+int _index = -1;
+unsigned int dpad = 0;
+unsigned int buttons = 0;
+int axisY = 0;
+int axisRX = 0;
+int throttle = 0;
+
 
 //===CODE SECTION===
 void setup() {
     Serial.begin(115200);
 
     //===SERVO SETUP===
-    Serial.println("\nSetting up pitch and yaw servos...")
-    pitch.attatch(PITCH); // initialize pitch servo
-    yaw.attatch(YAW); // initialize yaw servo
+    Serial.println("\nSetting up pitch and yaw servos...");
+    pitch.attach(PITCH); // initialize pitch servo
+    yaw.attach(YAW); // initialize yaw servo
     pitch.write(90); // positioning pitch servo in neutral position
     yaw.write(90); // positioning yaw servo in neutral position
-    Serial.println("\nServos setup finished.")
+    Serial.println("\nServos setup finished.");
 
     //===WIRELESS CONNECTION SETUP===
     Serial.println("\nSetting up NRF24L01...");
     radio.begin();
     radio.openReadingPipe(0, address);
     radio.setPALevel(RF24_PA_MIN);  // radio.setPALevel(RF24_PA_MIN);
-    Serial.println("\nNR24L01 setup finished.")
+    Serial.println("\nNR24L01 setup finished.");
 }
 
 //==============
@@ -46,9 +54,11 @@ void setup() {
 void loop() {
     radio.startListening();
     while (radio.available()) {
-    Serial.print("\nReceived");
+    Serial.print("\nReceived: ");
     getData();
-    showData();
+    //Serial.println(dataReceived);
+    processData();
+    //showData();
   }
 }
 
@@ -66,8 +76,36 @@ void showData()
 {
     if (newData == true)
     {
-        Serial.print("[RECEIVED DATA]: ");
         Serial.println(dataReceived);
         newData = false;
     }
+}
+
+// process the received data
+void processData() {
+
+  for (int i = 0; i < strlen(dataReceived); i++) {
+    if (dataReceived[i] == '\n' || dataReceived[i] == '\r') {
+      dataReceived[i] = '\0';
+    }
+  }
+
+  int parsed = sscanf(dataReceived, "0x%x|0x%x|%d|%d|%d|",
+                      &dpad, &buttons, &axisY, &axisRX, &throttle);
+
+
+  Serial.print("D-pad: ");
+  Serial.print(dpad, HEX);
+
+  Serial.print(" Buttons: ");
+  Serial.print(buttons, HEX);
+
+  Serial.print(" Axis Y: ");
+  Serial.print(axisY);
+
+  Serial.print(" Axis RX: ");
+  Serial.print(axisRX);
+
+  Serial.print(" Throttle: ");
+  Serial.print(throttle);
 }
